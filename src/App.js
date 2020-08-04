f020c8470ab98998d775e3caea124b6564176812
kamwanasamwel <kamwanasamwel@gmail.com>
2020-08-04 11:26:00 +0300

    setup up bootstrap configuration and simple test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import { PizzaCard } from './Components/PizzaCards';
+import { Confirmation } from './Components/Confirmation';
+import pizzas from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <p>Hello there</p>
+        </Row>
+      </Container>
     </div>
   );
 }
